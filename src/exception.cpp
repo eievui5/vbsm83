bbbfdf83e985d3d7b6cb775f57a7eb4ac0bef34f
Eievui <14899090+GreenAndEievui@users.noreply.github.com>
2021-11-10 18:07:35 -0500

    Added `info()`

--- src/exception.cpp
+++ src/exception.cpp
@@ -9,6 +9,22 @@ unsigned error_count = 0;
  * Warnings should warn the iser of strange or unsafe behavior that does not
  * prevent compilation.
 */
+void info(char const *fmt, ...) {
+    if (!enable_info)
+        return;
+    va_list ap;
+
+    fputs("\033[1m\033[93minfo: \033[0m", stdout);
+    va_start(ap, fmt);
+    vfprintf(stdout, fmt, ap);
+    va_end(ap);
+    putc('\n', stdout);
+}
+
+/* Print a warning message to stderr.
+ * Warnings should warn the iser of strange or unsafe behavior that does not
+ * prevent compilation.
+*/
 void warn(char const *fmt, ...) {
     va_list ap;
 
--- src/include/exception.hpp
+++ src/include/exception.hpp
@@ -5,6 +5,7 @@
 extern bool enable_info;
 extern unsigned error_count;
 
+void info(char const *fmt, ...);
 void warn(char const *fmt, ...);
 void error(char const *fmt, ...);
 void fatal(char const *fmt, ...);
