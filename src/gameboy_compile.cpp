09e339c1fd77f3972cc6f14d8d0fcf9720fe7f66
Eievui <14899090+GreenAndEievui@users.noreply.github.com>
2021-11-13 23:36:39 -0500

    Add debug assembly  comments

--- src/gameboy_compile.cpp
+++ src/gameboy_compile.cpp
@@ -3,25 +3,49 @@
 #include "statements/label.hpp"
 #include "statements/local_var.hpp"
 #include "statements/return.hpp"
+#include "types.hpp"
 
 void Label::compile(std::ostream& outfile) {
-    outfile << '_';
-    if (storage_class == StorageClass::STATIC)
+    // Output an info comment.
+    outfile << "; " << STORAGE_CLASS[(int) storage_class] << ' ' << identifier << " [[ ";
+    for (auto& i : trait_list) {
+        outfile << i << ", ";
+    }
+    outfile << "]]\n";
+
+    // Output label if needed.
+    if (storage_class != StorageClass::EXTERN) {
         outfile << '_';
-    outfile << identifier << ':';
-    if (storage_class == StorageClass::EXPORT)
-        outfile << ':';
-    outfile << '\n';
+        if (storage_class == StorageClass::STATIC)
+            outfile << '_';
+        outfile << identifier << ':';
+        if (storage_class == StorageClass::EXPORT)
+            outfile << ':';
+        outfile << '\n';
+    }
+}
+
+void Variable::compile(std::ostream& outfile) {
+    Label::compile(outfile);
+    outfile << "\tDS " << get_type(variable_type).size << '\n';
+}
+
+void Function::compile(std::ostream& outfile) {
+    Label::compile(outfile);
+    for (auto* i : unit_block.statements) {
+        i->compile(outfile);
+    }
 }
 
 void LocalVarDeclaration::compile(std::ostream& outfile) {
-    outfile << '_';
+    outfile << "\t; " << get_type(variable_type).str << ' ' << identifier << '\n';
 }
 
 void LocalVarAssignment::compile(std::ostream& outfile) {
-    outfile << '_';
+    outfile << "\t; " << identifier << " = " << value << '\n';
 }
 
 void ReturnStatement::compile(std::ostream& outfile) {
-    outfile << '_';
+    outfile << "\t; return " << value << '\n';
+    outfile << "\tret\n";
 }
--- src/statements/label.hpp
+++ src/statements/label.hpp
@@ -18,10 +18,14 @@ class Function : public Label {
   public:
     UnitContext unit_block;
 
+    void compile(std::ostream& outfile);
+
     ~Function() = default;
 };
 
 class Variable : public Label {
   public:
+    void compile(std::ostream& outfile);
+
     ~Variable() = default;
 };
