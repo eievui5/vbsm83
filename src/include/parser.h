31f661c1ea2a7a8aae5e184a137f109e9dbea860
Eievui <14899090+GreenAndEievui@users.noreply.github.com>
2022-01-09 17:44:51 -0500

    Add `strequ()`

--- src/include/parser.h
+++ src/include/parser.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "statements.h"
 
@@ -9,3 +11,7 @@ extern const char* STORAGE_CLASS[];
 extern const char* TYPE[];
 
 Declaration** fparse_textual_ir(FILE* infile);
+
+static inline bool strequ(const char* s1, const char* s2) {
+    return strcmp(s1, s2) == 0;
+}
